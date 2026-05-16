ac701ae182370cfb55fe53dbc01410e248e1336b
happyrilla <basukabomberumpe@gmail.com>
2026-05-16 15:10:14 +0200

    better code

--- js/facts.js
+++ js/facts.js
@@ -1,6 +1,6 @@
 // i found this script online. this isnt mine. you can take it if you want
 document.addEventListener("DOMContentLoaded", function () {
-  fetch("/assets/facts.JSON")
+  fetch("/The-Hub/assets/facts.JSON")
     .then((response) => response.json())
     .then((data) => {
       const randomQuote = data[Math.floor(Math.random() * data.length)];
